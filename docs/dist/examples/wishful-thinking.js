534ad10fc091a3bd32555808278bf49656777f43
kurtbruns <kurtjamesbruns@gmail.com>
2019-08-13 01:54:56 -0600

    test

--- docs/dist/examples/wishful-thinking.js
+++ docs/dist/examples/wishful-thinking.js
@@ -7,4 +7,4 @@
 // controls the center position of the circle, and then if they click or hover
 // over the text "this input" the control gets highlighted within the
 // interactive
-//# sourceMappingURL=wishful-thinking.js.map
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoid2lzaGZ1bC10aGlua2luZy5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uL3NvdXJjZS9leGFtcGxlcy93aXNoZnVsLXRoaW5raW5nLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUNBLHFCQUFxQjtBQUNyQixxQ0FBcUM7QUFDckMsNkNBQTZDO0FBQzdDLDBCQUEwQjtBQUUxQix5RUFBeUU7QUFDekUsMEVBQTBFO0FBQzFFLDhFQUE4RTtBQUM5RSxxRUFBcUU7QUFDckUsY0FBYyJ9
