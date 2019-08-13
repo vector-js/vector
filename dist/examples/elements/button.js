f2457c40c1b6618b19e37466c21728ac0881f6b0
kurtbruns <kurtjamesbruns@gmail.com>
2019-08-13 01:24:05 -0600

    changed urls in interactives

---
+++ dist/examples/elements/button.js
@@ -0,0 +1,9 @@
+// import Interactive from 'https://unpkg.com/@interactive-svg/library/dist/Interactive.js';
+import Interactive from '../../Interactive.js';
+import { getScriptName } from '../../Util.js';
+// Initialize the interactive
+let interactive = new Interactive(getScriptName());
+interactive.width = 300;
+interactive.height = 150;
+let button = interactive.button(100, 75, 'My Button');
+//# sourceMappingURL=button.js.map
