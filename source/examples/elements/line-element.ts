32ead4395fa4f7067eda7811d0092ffaf1f6af63
kurtbruns <kurtjamesbruns@gmail.com>
2019-08-13 01:31:31 -0600

    fixed elements id and script paths

---
+++ source/examples/elements/line-element.ts
@@ -0,0 +1,7 @@
+import Interactive from '../../Interactive.js';
+import { getScriptName } from '../../Util.js';
+let interactive = new Interactive(getScriptName());
+interactive.width = 768;
+interactive.height = 150;
+interactive.svg.style.border = "1px solid grey";
+let line = interactive.line( 50, 25, 150, 125);
