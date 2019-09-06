63049baef5cca65582373592fc46317e92df72db
Joshua Homer <Joshua@Joshuas-MacBook-Pro-3.local>
2019-09-06 12:21:09 -0600

    fix file hierarchy

--- source/examples/elements/map-element.ts
+++ source/examples/elements/map-element.ts
@@ -1,8 +1,10 @@
 import Interactive from '../../Interactive.js';
 import { getScriptName } from '../../Util.js';
+
 let interactive = new Interactive(getScriptName());
 interactive.root.style.border = "1px solid grey";
-let map = interactive.map("canada", 768, 300);
+let map = interactive.map("canada",768,300);
+
 let inputContainer = document.createElement('div');
 inputContainer.style.width = '100%';
 inputContainer.style.height = '2rem';
@@ -20,17 +22,17 @@ input.style.borderRadius = '4px';
 input.style.fontSize = '14px';
 interactive.container.parentElement.insertBefore(inputContainer, interactive.container);
 inputContainer.appendChild(input);
-input.addEventListener("keyup", function (event) {
+
+input.addEventListener("keyup", function(event) {
     // Number 13 is the "Enter" key on the keyboard
     if (event.keyCode === 13) {
         let bbb = document.getElementById('map-element');
         let t = document.getElementsByClassName('country');
         let stop = t.length;
         let i = 0;
-        for (i = 0; i < stop; i = (i + 1) % t.length) {
+        for(i = 0; i < stop; i = (i+1)%t.length){
             bbb.children[0].children[0].removeChild(t[i]);
         }
-        map = interactive.map(input.value, 768, 300);
+      map = interactive.map(input.value,768,300);
     }
-});
-//# sourceMappingURL=map-element.js.map
+  });
