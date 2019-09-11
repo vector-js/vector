81338a5d8a1a010c9136dbf23ad3378cf4d7ae22
Zachary Cutler <zacharycutler@Zacharys-MacBook-Pro.local>
2019-09-11 01:19:31 -0600

    initial mergesort stuff, need to figure out how to find the parents

--- dist/examples/math/mergesort.js
+++ dist/examples/math/mergesort.js
@@ -5,9 +5,10 @@ interactive.width = 704;
 interactive.height = 400;
 interactive.border = true;
 let graph = interactive.graph();
+let allArrays = [];
 // this HTML input element controls the current tree being drawn
 let input = document.createElement('input');
-input.type = 'number';
+input.type = 'textfield';
 input.style.width = '704px';
 input.value = '60';
 input.name = 'number';
@@ -19,30 +20,30 @@ input.style.webkitAppearance = 'textfield';
 input.style.fontSize = '14px';
 interactive.container.parentElement.insertBefore(input, interactive.container);
 input.onchange = function () {
-    drawGraph();
+    let arr = input.value.split(/[ ,]+/);
+    let numArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        numArr.push(parseInt(arr[i]));
+    }
+    createArrays(numArr);
+    console.log(allArrays);
 };
-function drawGraph() {
-    // remove all the graph elements
+function drawGraph(levels) {
     graph.clear();
-    // redraw the prime factorization tree
-    primeFactors(parseInt(input.value), 0, 0, 0, null);
-    let rect = graph.root.getBBox();
-    console.log(graph.size());
-    if (graph.size() == 1) {
-        interactive.setViewBox(rect.x - 32, rect.y - 32, rect.width + 64, rect.height + 64);
+    for (let i = 0; i < allArrays.length; i++) {
     }
-    else {
-        interactive.setViewBox(rect.x - 8, rect.y - 8, rect.width + 16, rect.height + 16);
-    }
+    // let rect = (graph.root as SVGGraphicsElement).getBBox();
+    //
+    // console.log(graph.size());
+    // if(graph.size() == 1)
+    // {
+    //   interactive.setViewBox(rect.x-32, rect.y-32, rect.width + 64, rect.height + 64)
+    // }
+    // else{
+    //   interactive.setViewBox(rect.x-8, rect.y-8, rect.width + 16, rect.height + 16)
+    // }
 }
-// draw the initial prime factorization tree for the current input
-let radius = 30;
-drawGraph();
-/**
-* This is a recursive function that draws the prime factorization tree for the
-* input number n.
-*/
-function primeFactors(n, p, x, y, prev) {
+function drawVis(n, p, x, y, prev) {
     // base case
     if (n == p || n <= 1) {
         let leaf = graph.addNode(x, y, n.toString(), radius);
@@ -66,6 +67,58 @@ function primeFactors(n, p, x, y, prev) {
     else {
         primeFactors(n, nextPrime(p), x, y, prev);
     }
+}
+// draw the initial prime factorization tree for the current input
+/**
+* This is a recursive function that draws the prime factorization tree for the
+* input number n.
+*/
+function createArrays(list) {
+    allArrays = [];
+    mergeSort(list, 0);
+}
+export function mergeSort(array, level) {
+    if (array.length <= 1) {
+        return array;
+    }
+    const middle = Math.floor(array.length / 2);
+    const left = array.slice(0, middle);
+    const right = array.slice(middle);
+    if (allArrays[level] === undefined) {
+        allArrays.push([left, right]);
+    }
+    else {
+        allArrays[level].push(left);
+        allArrays[level].push(right);
+    }
+    return merge(mergeSort(left, level + 1), mergeSort(right, level + 1));
+}
+/** Merge (conquer) step of mergeSort */
+function merge(left, right) {
+    const array = [];
+    let lIndex = 0;
+    let rIndex = 0;
+    while (lIndex + rIndex < left.length + right.length) {
+        const lItem = left[lIndex];
+        const rItem = right[rIndex];
+        if (lItem == null) {
+            array.push(rItem);
+            rIndex++;
+        }
+        else if (rItem == null) {
+            array.push(lItem);
+            lIndex++;
+        }
+        else if (lItem < rItem) {
+            array.push(lItem);
+            lIndex++;
+        }
+        else {
+            array.push(rItem);
+            rIndex++;
+        }
+    }
+    return array;
 }
 // add a save function to the window to save the current prime factorization
 // tree
