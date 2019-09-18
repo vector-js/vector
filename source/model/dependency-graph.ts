6ab55227765939bed6433fd7a52f3bed96dfba83
Kurt Bruns <kurtjamesbruns@gmail.com>
2019-09-18 11:11:10 -0600

    added topological sort ot dependency graph

--- source/model/dependency-graph.ts
+++ source/model/dependency-graph.ts
@@ -3,8 +3,7 @@ import LinkedList from './linked-list.js';
 /**
 A dependency graph models relationships between nodes. The graph is directed and asyclic, throwing a circular dependency exception if circular dependencies are added.
 */
-export default class DependencyGraph<T>
-{
+export default class DependencyGraph<T> {
 
 	// Stores the relationships between nodes
 	private relationships : Map< T, Set<T>>;
@@ -15,8 +14,7 @@ export default class DependencyGraph<T>
 	/**
 	Constructs an empty dependency graph.
 	*/
-	constructor()
-	{
+	constructor() {
 		this.relationships = new Map<T, Set<T>>();
 		this._size = 0;
 	}
@@ -24,10 +22,8 @@ export default class DependencyGraph<T>
 	/***
 	Adds a node into the dependency graph. If the node already exists within the graph, does nothing.
 	*/
-	add( node:T ) : void
-	{
-		if( !this.contains(node) )
-		{
+	add( node:T ) : void {
+		if( !this.contains(node) ) {
 			this.relationships.set( node, new Set<T>());
 			this._size++;
 		}
@@ -36,16 +32,14 @@ export default class DependencyGraph<T>
 	/**
 	Returns true if the node exists within the dependency graph.
 	*/
-	contains( node:T ) : boolean
-	{
+	contains( node:T ) : boolean {
 		return this.relationships.has(node);
 	}
 
 	/**
 	Removes the node from the dependency graph. If the node does not exist does nothing.
 	*/
-	remove( node:T ) : void
-	{
+	remove( node:T ) : void {
 		if (this.relationships.delete(node))
 		{
 			this._size--;
@@ -55,16 +49,14 @@ export default class DependencyGraph<T>
 	/**
 	Returns the number of vertices in the dependency graph.
 	*/
-	size() : number
-	{
+	size() : number {
 		return this._size;
 	}
 
 	/**
 	Adds a dependency between two nodes. If either of the nodes do not exist within the dependency graph, throws an exception.
 	*/
-	addDependency( from:T, to:T) : void
-	{
+	addDependency( from:T, to:T) : void {
 		// Make sure the nodes exist
 		this.add(from);
 		this.add(to);
@@ -79,24 +71,20 @@ export default class DependencyGraph<T>
 	/**
 	Traverses the graph structuring checking for circular dependecies. If a circular dependency is added, throws an error.
 	*/
-	private traverse( current:T, node:T, visited:Set<T> = new Set<T>()) : void
-	{
+	private traverse( current:T, node:T, visited:Set<T> = new Set<T>()) : void {
 		// Mark this node as visited
 		visited.add(current);
 
 		// Recursively call this method on dependents of the argument node
 		let dependents = this.getDependents( current, true);
-		for( let d of dependents )
-		{
+		for( let d of dependents ) {
 			// Check if this dependency causes a circular dependency
-			if( d == node )
-			{
+			if( d == node ) {
 				throw new Error("circular dependency");
 			}
 
 			// Continue traversing un-explored nodes
-			if(!visited.has(d))
-			{
+			if(!visited.has(d)) {
 				this.traverse(d, node, visited);
 			}
 		}
@@ -105,29 +93,23 @@ export default class DependencyGraph<T>
 	/**
 	Returns true if a node has dependents.
 	*/
-	hasDependents( node:T ) : boolean
-	{
+	hasDependents( node:T ) : boolean {
 		return this.contains(node) && this.relationships.get( node).size != 0;
 	}
 
 	/**
 	* Returns an iterator to the dependents of the node.
 	*/
-	getDependents( node:T, shallow:boolean = false ) : Iterable<T>
-	{
+	getDependents( node:T, shallow:boolean = false ) : Iterable<T> {
 		// If the node does not exist return an empty iterable
-		if( !this.relationships.has(node))
-		{
+		if( !this.relationships.has(node)) {
 			return [];
 		}
 
 		// If shallow, return adjacent dependencies.
-		if( shallow )
-		{
+		if( shallow ) {
 			return this.relationships.get(node).keys();
-		}
-		else
-		{
+		} else {
 			// Get the dependents including the original node.
 			let list : LinkedList<T> = this.getTopologicalDependents( node );
 
@@ -138,19 +120,32 @@ export default class DependencyGraph<T>
 	}
 
 	/**
+	* Returns a topological sort of this dependency
+	*/
+	getTopologicalSort() : LinkedList<T> {
+
+		let list = new LinkedList<T>();
+		let visited = new Set<T>();
+		for (let node of this.getNodes()) {
+			if( !visited.has(node)) {
+				this.getTopologicalDependents(node, visited, list);
+			}
+		}
+
+		return list;
+	}
+
+	/**
 	Returns a list of the arguent node and all of its dependents in topological order.
 	*/
-	private getTopologicalDependents( node:T, visited:Set<T> = new Set<T>(), list:LinkedList<T> = new LinkedList()) : LinkedList<T>
-	{
+	private getTopologicalDependents( node:T, visited:Set<T> = new Set<T>(), list:LinkedList<T> = new LinkedList<T>()) : LinkedList<T> {
 		// Mark this node as visited
 		visited.add(node);
 
 		// Recursively call this method on dependents of the argument node
 		let dependents = this.getDependents( node, true);
-		for( let d of dependents )
-		{
-			if(!visited.has(d))
-			{
+		for( let d of dependents ) {
+			if(!visited.has(d)) {
 				this.getTopologicalDependents(d, visited, list);
 			}
 		}
@@ -163,22 +158,18 @@ export default class DependencyGraph<T>
 	/**
 	Returns the nodes within this dependency graph.
 	*/
-	getNodes() : Iterable<T>
-	{
+	getNodes() : Iterable<T> {
 		return this.relationships.keys();
 	}
 
 	/**
 	Returns a string representation of this dependency graph.
 	*/
-	toString() : string
-	{
+	toString() : string {
 		// Build a string of dependencies in the form of from->to
 		let result = "";
-		for( let from of this.getNodes() )
-		{
-			for( let to of this.getDependents(from, true) )
-			{
+		for( let from of this.getNodes() ) {
+			for( let to of this.getDependents(from, true) ) {
 				result += from.toString() + '->' + to.toString() + '\n';
 			}
 		}
@@ -188,15 +179,14 @@ export default class DependencyGraph<T>
 	/**
 	Generates a DependenyGraph object from a string representation.
 	*/
-	static Generate( str:string ) : DependencyGraph<string>
-	{
+	static Generate( str:string ) : DependencyGraph<string> {
 		let graph = new DependencyGraph<string>();
 
 		// Prime the loop
 		let start = 0;
 		let index = str.indexOf('->', start);
-		while( index > 0)
-		{
+		while( index > 0) {
+
 			// Get the first part of the dependency
 			let from = str.substring(start, index);
 
--- source/model/linked-list.ts
+++ source/model/linked-list.ts
@@ -1,48 +1,47 @@
 /**
 * A node class contains data and a recursive next point.
 */
-class Node<T>
-{
+class Node<T> {
 	next : Node<T>;
   data: T;
 
 	/**
 	Constructs a new node with the provided data and sets next to be null.
 	*/
-  constructor( data: T)
-  {
+  constructor( data: T) {
     this.data = data;
     this.next = null;
   }
+
+	/**
+	* Returns the string representation of the data.
+	*/
+	toString() : string {
+		return this.data.toString();
+	}
 }
 
 /**
 * A dynamic, singlely linked list.
 */
-export default class LinkedList<T> implements Iterable<T>
-{
+export default class LinkedList<T> implements Iterable<T> {
 	// Contains head of linked list
 	private head: Node<T>;
 
 	/**
 	Consstructs an empty linked list.
 	*/
-	constructor()
-	{
+	constructor() {
 		this.head = null;
 	}
 
 	/**
 	Inserts a node at the beginning of the list
 	*/
-	insert( element:T )
-	{
-		if( this.head == null )
-		{
+	insert( element:T ) {
+		if( this.head == null ) {
 			this.head = new Node(element);
-		}
-		else
-		{
+		} else {
 			let temp = this.head;
 			this.head = new Node(element);
 			this.head.next = temp;
@@ -52,14 +51,10 @@ export default class LinkedList<T> implements Iterable<T>
 	/**
 	Returns the first element in the list, or null if the list is empty.
 	*/
-	first() : T
-	{
-		if( this.head != null)
-		{
+	first() : T {
+		if( this.head != null) {
 			return this.head.data;
-		}
-		else
-		{
+		} else {
 			return null;
 		}
 	}
@@ -67,38 +62,42 @@ export default class LinkedList<T> implements Iterable<T>
 	/**
 	Removes the first element in the list. Returns true if element was successfully removed, false otherwise.
 	*/
-	remove() : boolean
-	{
-		if( this.head != null )
-		{
+	remove() : boolean {
+		if( this.head != null ) {
 			this.head = this.head.next;
 			return true;
-		}
-		else
-		{
+		} else {
 			return false;
 		}
 	}
 
 	/**
+	* Prints out the string reprsentation of this linked list.
+	*/
+	toString() : string {
+		let current = this.head;
+		let str = '';
+		while( current != null) {
+			str += current.toString() + ' ';
+			current = current.next;
+		}
+		return str.substr(0, str.length - 1);
+	}
+
+	/**
 	Returns an iterator over the elements in the list
 	*/
-	[Symbol.iterator](): Iterator<T>
-	{
+	[Symbol.iterator](): Iterator<T> {
 		let current = this.head;
 
 		const iterator = {
-			next()
-			{
-				if( current == null)
-				{
+			next() {
+				if( current == null) {
 					return {
 						done: true,
 						value: undefined
 					}
-				}
-				else
-				{
+				} else {
 					let data = current.data;
 					current = current.next;
 					return {
