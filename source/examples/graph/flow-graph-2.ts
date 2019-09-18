8a10a4044d1ab39496c63d9a8e6536de04b44809
Kurt Bruns <kurtjamesbruns@gmail.com>
2019-09-18 11:35:49 -0600

    added flow graph example

---
+++ source/examples/graph/flow-graph-2.ts
@@ -0,0 +1,9 @@
+import Interactive from '../../interactive.js';
+import { getScriptName } from '../../util.js';
+
+let interactive = new Interactive(getScriptName());
+let graph = `a->b
+b->c
+a->c`;
+
+interactive.flowGraph(graph);
