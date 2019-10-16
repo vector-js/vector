41abfa804efd89c545ef8a46dfce705ae84335a2
Kurt Bruns <kurtjamesbruns@gmail.com>
2019-10-16 12:35:48 -0600

    actually removed generated docs

--- docs/elements/svg/content-model.js
+++
@@ -1,7 +0,0 @@
-import Element from './element.js';
-// NOTE: these interfaces only extend the Element object for testing purposes.
-/*
-* Export the general SVG element
-*/
-export { Element };
-//# sourceMappingURL=content-model.js.map
--- docs/interactive.js
+++
@@ -1,3 +0,0 @@
-import Interactive from "./elements/interactive.js";
-export default Interactive;
-//# sourceMappingURL=interactive.js.map
