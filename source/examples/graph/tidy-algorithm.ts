50891a75e81655d7fccd5dce6fe862e4ba5a2c39
kurtbruns <kurtjamesbruns@gmail.com>
2019-11-26 14:46:01 -0700

    renamed tidy algorithm example

--- source/examples/graph/tidy-algorithm.ts
+++ source/examples/graph/tidy-algorithm.ts
@@ -6,9 +6,7 @@
 * @weight 1
 */
 
-import { nextPrime } from '../../util/math.js';
 import {Interactive, getScriptName} from '../../index.js';
-import Node from '../../elements/graph/node.js';
 
 let interactive = new Interactive(getScriptName());
 interactive.width = 736;
